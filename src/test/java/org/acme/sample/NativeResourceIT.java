464feaed4418a69309cd22b2d830da4f6a19f623
Anton Kozlov <akozlov@azul.com>
2020-06-04 10:55:22 +0300

    Import quarkus sample project
    mvn io.quarkus:quarkus-maven-plugin:1.5.0.Final:create \
    	-DprojectArtifactId=example-quarkus \
    	-DclassName="org.acme.sample.Resource"

---
+++ src/test/java/org/acme/sample/NativeResourceIT.java
@@ -0,0 +1,9 @@
+package org.acme.sample;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeResourceIT extends ResourceTest {
+
+    // Execute the same tests but in native mode.
+}
